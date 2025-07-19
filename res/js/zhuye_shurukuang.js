4b2cfd3d4715b6fea4bfb1aee2f529b4522c406c
沛霖 霍 <apkqiu@qq.com>
2025-07-19 21:10:47 +0800

    优化HTML结构及样式

--- res/js/zhuye_shurukuang.js
+++ res/js/zhuye_shurukuang.js
@@ -2,27 +2,27 @@
 const fka = document.getElementById('fka');
 
 // 添加输入事件监听器
-srka.addEventListener('input', function() {
+srka.addEventListener('input', function () {
     // 获取输入的值并转换为小写方便比较
     const value = srka.value.toLowerCase();
-    
+
     // 清空之前的效果
     fka.textContent = '';
     fka.style.backgroundColor = '#f9f9f9';
-    
+
     if (value === 'zhoubao') {
         fka.textContent = '周恩来周报';
         fka.style.backgroundColor = '#d4edda';
-        window.location.href= 'zhoubao/zhuye zhoubao.html';
+        window.location.href = 'zhoubao/zhuye zhoubao.html';
     } else if (value === 'yicaozhong') {
-        window.location.href= 'http://sz1cz.gusuedu.cn/index.htm';
+        window.location.href = 'http://sz1cz.gusuedu.cn/index.htm';
         fka.textContent = '一草种';
     } else if (value === 'game') {
-        window.location.href= 'game/game.html';
+        window.location.href = 'game/game.html';
         fka.style.backgroundColor = '#fff3cd';
         fka.textContent = 'game';
-    } else if (value === 'nihao')  {
-        window.location.href= 'zhuye.html';
+    } else if (value === 'nihao') {
+        window.location.href = 'zhuye.html';
         fka.style.backgroundColor = '#cce5ff';
     } else if (value.trim() !== '') {
         fka.textContent = `你输入了: ${aa.value}`;
