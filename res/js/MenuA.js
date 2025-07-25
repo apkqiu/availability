838240df2d1c8c85cd58a190fe92ab02fd628a61
沛霖 霍 <apkqiu@qq.com>
2025-07-25 20:20:00 +0800

    ljh上传

---
+++ res/js/MenuA.js
@@ -0,0 +1,15 @@
+﻿  document.getElementById("MenuAPlace").innerHTML="      <button type=\"button\" id=\"MenuABtn\" onclick=\"openMenuA()\">菜单<\/button>";
+function openMenuA()
+{
+  document.getElementById("MenuAArea").innerHTML="      <div><b>洽隐山房&nbsp;菜单<\/b>      <br><br>      <button type=\"button\" onclick=\"closeMenuA()\">关闭菜单<\/button>      <button type=\"button\" onclick=\"zhuye()\">返回主页<\/button>      <\/div>      <div id=\"MenuAShadow\"><\/div>";
+  document.getElementById("MenuAArea").style.display="block";
+  document.getElementById("MenuAShadow").style.opacity="0.1";
+}
+function closeMenuA()
+{
+  document.getElementById("MenuAArea").style.display="none";
+}
+function zhuye()
+{
+  window.location.href="https://apkqiu.oc.com.ar/availability/zhuye.html"
+}
---
+++ res/js/searchA.js
@@ -0,0 +1,22 @@
+﻿function searchA(){
+  document.getElementById('SearchForm').innerHTML="";
+  var InputValue=document.getElementById('SearchText').value;
+  if (InputValue.includes('周')||InputValue.includes('恩')||InputValue.includes('来')||InputValue.includes('报')) {searchZhoubao();}
+  if (InputValue.includes('一')||InputValue.includes('草')||InputValue.includes('种')||InputValue.includes('初')||InputValue.includes('中')) {searchYicaozhong();}
+}
+function searchZhoubao(){
+  var SearchFormLi1=document.createElement('li');
+  var SearchFormLi1Link1=document.createElement('a');
+  SearchFormLi1Link1.href="zhoubao/index.html";
+  SearchFormLi1Link1.textContent="周恩来周报";
+  SearchFormLi1.appendChild(SearchFormLi1Link1);
+  document.getElementById('SearchForm').appendChild(SearchFormLi1);
+}
+function searchYicaozhong(){
+  var SearchFormLi2=document.createElement('li');
+  var SearchFormLi1Link2=document.createElement('a');
+  SearchFormLi1Link2.href="yicaozhong/index.html";
+  SearchFormLi1Link2.textContent="一草种";
+  SearchFormLi2.appendChild(SearchFormLi1Link2);
+  document.getElementById('SearchForm').appendChild(SearchFormLi2);
+}
