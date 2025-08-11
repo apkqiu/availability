76068030146d3b6e7e6cdcb4b9a74719248eb175
沛霖 霍 <apkqiu@qq.com>
2025-08-11 09:30:14 +0800

    添加旧版

--- 脚本/[生产]服务器.py
+++
@@ -1,7 +0,0 @@
-import os
-import 服务器 as server
-import waitress
-import logging
-logging.basicConfig(filename='app.log', encoding='utf-8', level=logging.DEBUG)
-waitress.serve(server.app, host='::', port=1111)
-# 替换成你的服务器地址和端口
