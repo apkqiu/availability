34894fa207052681ad876ac8e8def0d27b7dfaf6
沛霖 霍 <apkqiu@qq.com>
2025-09-07 10:40:14 +0800

    CI build

--- scripts/Python/ci.py
+++ scripts/Python/ci.py
@@ -10,3 +10,16 @@ execlib.exec_cmd("npm", "install",console=True)
 print("CI build environment initialized.")
 print("Starting CI build...")
 execlib.exec_cmd("python", "-u", os.path.join("scripts", "Python", "compile.py"),console=True)
+execlib.exec_cmd("git", "add", ".",console=True)
+execlib.exec_cmd("git", "commit", "-m", "CI build",console=True)
+while True:
+    # with proxy
+    execlib.exec_cmd("git", "config", "set","http.proxy", "127.0.0.1:10808",console=True)
+    ret = execlib.exec_cmd("git", "push",console=True)
+    if ret == 0:
+        break
+    # without proxy
+    execlib.exec_cmd("git", "config", "unset","http.proxy",console=True)
+    ret = execlib.exec_cmd("git", "push",console=True)
+    if ret == 0:
+        break
