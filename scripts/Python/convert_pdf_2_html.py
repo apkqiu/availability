ecbc59fb94650707186bab417a2daca220fe7449
沛霖 霍 <apkqiu@qq.com>
2025-09-08 22:54:41 +0800

    CI build

--- scripts/Python/convert_pdf_2_html.py
+++ scripts/Python/convert_pdf_2_html.py
@@ -1,24 +1,27 @@
+import concurrent
+import concurrent.futures
 import win32com.client
 import os
-import concurrent.futures
 for i in os.listdir("templates/news"):
     if i.endswith(".files"):
         os.system(f"rmdir /s /q {os.path.join('templates\\news', i)}")
     elif i.startswith("zhoubao"):
         os.remove(os.path.join("templates\\news", i))
-def convert_pdf_2_html(path):
-    print(i)
-    word = win32com.client.Dispatch("Word.Application")
-    word.Visible = False
-    doc = word.Documents.Open(path)
-    doc.SaveAs(os.path.join(os.path.dirname(path), os.path.basename(path).replace(".pdf", ".html")), FileFormat=8)
-    doc.Close()
-    word.Quit()
-    print("OK:",i)
-pool = concurrent.futures.ThreadPoolExecutor(max_workers=4) 
-# set as 4 because each convert needs a new Word.exe instance
+word = win32com.client.Dispatch("Word.Application")
+print("OPENING WORD")
+word.Visible = False
 for i in os.listdir("templates/res/pdf"):
     if i.startswith("~$"):
         continue
-    pool.submit(convert_pdf_2_html, os.path.join("templates/res/pdf", i))
-pool.shutdown()
+    fulpath = os.path.abspath(os.path.join("templates\\res\\pdf", i))
+    print("OPEN\t",i)
+    doc = word.Documents.Open(fulpath,ReadOnly=True)
+    print("SAVE\t", i)
+    doc.SaveAs(os.path.abspath(os.path.join("templates\\news", os.path.basename(fulpath).replace(".pdf", ".html"))), FileFormat=8)
+    print("CLOSE\t", i)
+    doc.Close()
+    print("OK\t",i)
+    #pool.submit(convert_pdf_2_html, os.path.abspath(os.path.join("templates\\res\\pdf", i)))
+print("ALL DONE")
+word.Quit()
+print("EXITING")
