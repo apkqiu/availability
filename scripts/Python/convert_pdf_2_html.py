eac4f3349742de1bb08b991bcd20a27ac2facdf4
沛霖 霍 <apkqiu@qq.com>
2025-09-08 22:43:57 +0800

    CI build

--- scripts/Python/convert_pdf_2_html.py
+++ scripts/Python/convert_pdf_2_html.py
@@ -1,34 +1,24 @@
 import win32com.client
 import os
+import concurrent.futures
 for i in os.listdir("templates/news"):
     if i.endswith(".files"):
         os.system(f"rmdir /s /q {os.path.join('templates\\news', i)}")
     elif i.startswith("zhoubao"):
         os.remove(os.path.join("templates\\news", i))
 def convert_pdf_2_html(path):
+    print(i)
     word = win32com.client.Dispatch("Word.Application")
     word.Visible = False
     doc = word.Documents.Open(path)
-    doc.SaveAs(os.path.join(os.path.dirname(path), os.path.basename(path).replace(".pdf", ".html")), FileFormat=17)
+    doc.SaveAs(os.path.join(os.path.dirname(path), os.path.basename(path).replace(".pdf", ".html")), FileFormat=8)
     doc.Close()
     word.Quit()
+    print("OK:",i)
+pool = concurrent.futures.ThreadPoolExecutor(max_workers=4) 
+# set as 4 because each convert needs a new Word.exe instance
 for i in os.listdir("templates/res/pdf"):
     if i.startswith("~$"):
         continue
-    print(i)
-    path = os.path.abspath(os.path.join("templates/res/pdf", i))
-    out = os.path.abspath(os.path.join("templates/news/", i.replace(".pdf", ".html")))
-    # 创建 Word 应用程序对象
-    word = win32com.client.Dispatch('Word.Application')
-    word.Visible = 0 # 后台运行
-    word.DisplayAlerts = 0 # 不显示警告
-
-    # 打开已有的 Word 文档
-    doc = word.Documents.Open(path)
-
-    # 将文档另存为 HTML 格式
-    doc.SaveAs(out, FileFormat=8,Encoding=65001)
-
-    # 关闭文档和 Word 应用程序
-    doc.Close()
-    print("OK:",i)
+    pool.submit(convert_pdf_2_html, os.path.join("templates/res/pdf", i))
+pool.shutdown()
