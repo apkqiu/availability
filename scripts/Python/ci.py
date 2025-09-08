6ed908009a3eced4c67c2611bc6a9bc590a4a02a
沛霖 霍 <apkqiu@qq.com>
2025-09-08 22:39:24 +0800

    CI build

--- scripts/Python/ci.py
+++ scripts/Python/ci.py
@@ -1,14 +1,7 @@
 import lib.execlib as execlib
 import os
-import sys
-print("Getting ready for CI build...")
-print("Initalizing Python and Node.js environment...")
-print("Installing Python dependencies...")
-execlib.exec_cmd("pip", "install", "-r", "requirements.txt",console=True)
-print("Installing Node.js dependencies...")
-execlib.exec_cmd("npm", "install",console=True)
-print("CI build environment initialized.")
-print("Starting CI build...")
+execlib.exec_cmd("python", "-u", os.path.join("scripts", "Python", "fix_encoding.py"),console=True)
+execlib.exec_cmd("python", "-u", os.path.join("scripts", "Python", "fix_encoding.py"),console=True)
 execlib.exec_cmd("python", "-u", os.path.join("scripts", "Python", "compile.py"),console=True)
 execlib.exec_cmd("git", "add", ".",console=True)
 execlib.exec_cmd("git", "commit", "-m", "CI build",console=True)
