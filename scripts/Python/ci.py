aaca47298617fc4d4b67c83e0e4cbeabc0b8cb0d
沛霖 霍 <apkqiu@qq.com>
2025-10-29 22:01:33 +0800

    build

--- scripts/Python/ci.py
+++ scripts/Python/ci.py
@@ -1,21 +1,28 @@
 import lib.execlib as execlib
 import os
+import sys
+local = "--local" in sys.argv
+server = "--server" in sys.argv
+if not local:
+    execlib.run("pip", "install", "-r", "requirements.txt")
+    execlib.run("npm", "i", "--include=dev")
 # execlib.run("python", os.path.join("scripts", "Python", "convert_pdf_2_html.py"))
 execlib.run("python", os.path.join("scripts", "Python", "compile.py"), "--recompile")
-execlib.run("git", "add", ".")
-execlib.run("git", "commit", "-m", "build")
-while True:
-    # with proxy
-    execlib.run("git", "config", "set","http.proxy", "127.0.0.1:10808")
-    try:
-        execlib.run("git", "push")
-        break
-    except:
-        pass
-    # without proxy
-    execlib.run("git", "config", "unset","http.proxy")
-    try:
-        execlib.run("git", "push")
-        break
-    except:
-        pass
+if not server:
+    execlib.run("git", "add", ".")
+    execlib.run("git", "commit", "-m", "build")
+    while True:
+        # with proxy
+        execlib.run("git", "config", "set","http.proxy", "127.0.0.1:10808")
+        try:
+            execlib.run("git", "push")
+            break
+        except:
+            pass
+        # without proxy
+        execlib.run("git", "config", "unset","http.proxy")
+        try:
+            execlib.run("git", "push")
+            break
+        except:
+            pass
