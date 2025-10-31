4127773ed14de08b9090c75195a1675aa1ce9497
沛霖 霍 <apkqiu@qq.com>
2025-10-31 21:50:38 +0800

    build

--- configure.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-pushd %~dp0
-python scripts/python/configure.py --configure
-popd
--- node/main.js
+++ node/main.js
@@ -1,6 +1,6 @@
-const { app, BrowserWindow, Menu } = require('electron')
+const { app, BrowserWindow } = require('electron')
 
-const createWindow = () => {
+app.whenReady().then(() => {
     const win = new BrowserWindow({
         width: 1600,
         height: 900,
@@ -11,17 +11,4 @@ const createWindow = () => {
     })
     win.setMenu(null)
     win.loadFile('./docs/home.html')
-    a = setInterval(() => {
-        try{
-            if(win.title.startsWith("洽隐山房")) return;
-            win.title = "洽隐山房 - "+win.title;
-        }catch(e){
-            clearInterval(a)
-        }
-
-    },1)
-}
-
-app.whenReady().then(() => {
-    createWindow()
 })
